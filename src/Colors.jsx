973e126dddbfce72a3104131e87f39ee307ce787
AJP-ARCHIVE <angelajpark93@gmail.com>
2026-08-30 14:31:49 -0400

    Update Colors.jsx. Fixed spacing.
    Removed spacing between code blocks.

--- src/Colors.jsx
+++ src/Colors.jsx
@@ -4,24 +4,16 @@ import React from 'react'
 
 function Colors( { scheme }) {
 
- 
-
     // Map over prop 'scheme' to render color boxes 
     const colorBoxElement = scheme.map(color => <div key={color.id} className="color-box" style={{backgroundColor: color.hex}}></div>)
 
     return (
         <>
-   
         <section className="color-container">
          
             {colorBoxElement} 
           
-          
         </section>
-
-     
-
-
 
         </>
     )
